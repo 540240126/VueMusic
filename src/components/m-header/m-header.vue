ace8e93f4bb76e6e6b3089e96c7abbddd11cf8ca
duan <540240126@qq.com>
2017-09-19 14:41:47 +0800

    my first music app

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="m-header">
+    <div class="icon"></div>
+    <h1 class="text">Chicken Music</h1>
+    <router-link tag="div" class="mine" to="/user">
+      <i class="icon-mine"></i>
+    </router-link>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  @keyframes ide-bubble
+    0%,100%,40%,60%,
+    80%
+      transform:translate3d(0,0,0)
+    50%
+      transform:translate3d(0,-6px,0)
+    70%
+      transform:translate3d(0,-4px,0)
+    90%
+      transform:translate3d(0,-2px,0)
+
+  .m-header
+    position: relative
+    height: 44px
+    text-align: center
+    color: $color-theme
+    font-size: 0
+    .icon
+      display: inline-block
+      vertical-align: top
+      margin-top: 6px
+      width: 30px
+      height: 32px
+      margin-right: 9px
+      bg-image('logo')
+      background-size: 30px 32px
+      animation: ide-bubble 1.8s ease .3s infinite
+    .text
+      display: inline-block
+      vertical-align: top
+      line-height: 44px
+      font-size: $font-size-large
+    .mine
+      position: absolute
+      top: 0
+      right: 0
+      .icon-mine
+        display: block
+        padding: 12px
+        font-size: 20px
+        color: $color-theme
+</style>
